6938e32316f57361a7f477a17e8280cf732035fe
Connor Burton <me@connorburton.com>
2025-12-07 21:02:01 +0000

    export.sh and publish workflow

---
+++ export.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+set -u
+
+VAULT_EXPORT_DIR=/tmp/vault-export
+BW_CLIENTID=${BW_CLIENTID:-}
+BW_CLIENTSECRET=${BW_CLIENTSECRET:-}
+BW_PASSWORD=${BW_PASSWORD:-}
+EXPORT_FILE="$VAULT_EXPORT_DIR/bw-vault.json"
+
+mkdir -p "$VAULT_EXPORT_DIR"
+
+bw login --apikey --raw
+bw unlock --passwordenv BW_PASSWORD
+bw export --format encrypted_json --output "$EXPORT_FILE"
+
+echo "Vault exported to $EXPORT_FILE"
+
+bw lock
+
+unset BW_SESSION
