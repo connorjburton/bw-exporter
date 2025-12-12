b1a7a5d6155380855843eef53d7e94fc4b007af7
Connor Burton <me@connorburton.com>
2025-12-12 01:51:32 +0000

    Add lock and logout commands to export script

--- export.sh
+++ export.sh
@@ -12,3 +12,5 @@ mkdir -p "$VAULT_EXPORT_DIR"
 bw login --apikey --raw --nointeraction
 export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw --nointeraction)
 bw export --format encrypted_json --session "$BW_SESSION" --output "$VAULT_EXPORT_DIR" --nointeraction
+bw lock  --nointeraction
+bw logout  --nointeraction
