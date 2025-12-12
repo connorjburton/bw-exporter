a5a67860fc280e5f3409557e578d2ccf8f4a8a67
Connor Burton <me@connorburton.com>
2025-12-12 01:36:41 +0000

    Update export.sh

--- export.sh
+++ export.sh
@@ -6,14 +6,10 @@ VAULT_EXPORT_DIR=/backup
 BW_CLIENTID=${BW_CLIENTID:-}
 BW_CLIENTSECRET=${BW_CLIENTSECRET:-}
 BW_PASSWORD=${BW_PASSWORD:-}
-EXPORT_FILE="$VAULT_EXPORT_DIR/bw-vault.json"
 
 mkdir -p "$VAULT_EXPORT_DIR"
 
 bw login --apikey --raw
 export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
-#bw export --format encrypted_json --session "$BW_SESSION" --output "$EXPORT_FILE"
+bw export --format encrypted_json --session "$BW_SESSION" --output "$VAULT_EXPORT_DIR"
 
-echo "Vault exported to $EXPORT_FILE"
-
-tail -f /dev/null
