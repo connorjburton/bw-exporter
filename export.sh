9c79821f5f74d73c403f358f71c6f495ecf7b228
Connor Burton <me@connorburton.com>
2025-12-12 01:48:02 +0000

    Add --nointeraction flag to bw commands

--- export.sh
+++ export.sh
@@ -9,7 +9,6 @@ BW_PASSWORD=${BW_PASSWORD:-}
 
 mkdir -p "$VAULT_EXPORT_DIR"
 
-bw login --apikey --raw
-export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
-bw export --format encrypted_json --session "$BW_SESSION" --output "$VAULT_EXPORT_DIR"
-
+bw login --apikey --raw --nointeraction
+export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw --nointeraction)
+bw export --format encrypted_json --session "$BW_SESSION" --output "$VAULT_EXPORT_DIR" --nointeraction
