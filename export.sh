246f135904eb14a74cd286d3ce71523e30995e93
Connor Burton <me@connorburton.com>
2026-04-24 20:29:35 +0100

    Update export.sh

--- export.sh
+++ export.sh
@@ -2,15 +2,26 @@
 set -e
 set -u
 
-VAULT_EXPORT_DIR=/backup/
-BW_CLIENTID=${BW_CLIENTID:-}
-BW_CLIENTSECRET=${BW_CLIENTSECRET:-}
-BW_PASSWORD=${BW_PASSWORD:-}
-
+VAULT_EXPORT_DIR="/backup/"
+SECRET_PATH="/run/secrets/bw_password"
 mkdir -p "$VAULT_EXPORT_DIR"
 
-bw login --apikey --raw --nointeraction
+BW_CLIENTID=$(cat /run/secrets/bw_clientid)
+BW_CLIENTSECRET=$(cat /run/secrets/bw_clientsecret)
+BW_PASSWORD=$(cat /run/secrets/bw_password) # Keep local, don't export yet
+
+BW_CLIENTID="$BW_CLIENTID" BW_CLIENTSECRET="$BW_CLIENTSECRET" bw login --apikey --nointeraction
+
 export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw --nointeraction)
-bw export --format encrypted_json --session "$BW_SESSION" --output "$VAULT_EXPORT_DIR" --nointeraction
-bw lock  --nointeraction
-bw logout  --nointeraction
+
+unset BW_CLIENTID
+unset BW_CLIENTSECRET
+unset BW_PASSWORD
+
+bw export --format encrypted_json \
+          --session "$BW_SESSION" \
+          --output "$VAULT_EXPORT_DIR" \
+          --nointeraction
+
+unset BW_SESSION
+bw logout --nointeraction
